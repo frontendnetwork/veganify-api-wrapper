bb53562b8c367239ec42977018bcdda7987be4b2
Philip <4144601+philipbrembeck@users.noreply.github.com>
2023-08-15 16:34:28 +0200

    feat: Add Staging URL

--- lib/index.ts
+++ lib/index.ts
@@ -1,8 +1,14 @@
-const API_BASE_URL = 'https://api.vegancheck.me/v0';
+const PRODUCTION_API_BASE_URL = 'https://api.vegancheck.me/v0';
+const STAGING_API_BASE_URL = 'https://staging.api.vegancheck.me/v0';
+
+const getApiBaseUrl = (staging?: boolean): string => {
+  return staging ? STAGING_API_BASE_URL : PRODUCTION_API_BASE_URL;
+};
 
 const VeganCheck = {
-  getProductByBarcode: async (barcode: string) => {
+  getProductByBarcode: async (barcode: string, staging?: boolean) => {
     try {
+      const API_BASE_URL = getApiBaseUrl(staging);
       const response = await fetch(`${API_BASE_URL}/product/${barcode}`, {
         method: 'POST',
       });
@@ -17,8 +23,9 @@ const VeganCheck = {
     }
   },
 
-  checkIngredientsList: async (ingredientsList: string) => {
+  checkIngredientsList: async (ingredientsList: string, staging?: boolean) => {
     try {
+      const API_BASE_URL = getApiBaseUrl(staging);
       const response = await fetch(`${API_BASE_URL}/ingredients/${ingredientsList}`);
 
       if (!response.ok) {
@@ -31,8 +38,9 @@ const VeganCheck = {
     }
   },
 
-  getPetaCrueltyFreeBrands: async () => {
+  getPetaCrueltyFreeBrands: async (staging?: boolean) => {
     try {
+      const API_BASE_URL = getApiBaseUrl(staging);
       const response = await fetch(`${API_BASE_URL}/peta/crueltyfree`);
 
       if (!response.ok) {
